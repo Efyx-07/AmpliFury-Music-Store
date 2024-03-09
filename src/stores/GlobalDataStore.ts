eb87fde3937ebec8e4b46aac218703e6b5a15668
François-Xavier Savary <fx.savary@orange.fr>
2024-03-09 15:40:55 +0100

    GlobalDataStore

---
+++ src/stores/GlobalDataStore.ts
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia';
+
+const hostName: ImportMetaEnv = import.meta.env.VITE_BACKEND_URL;
+
+type State = {
+    hostName: ImportMetaEnv;
+    companyName: string;
+}
+
+export const useGlobalDataStore = defineStore('globalData', {
+
+    state: (): State => ({
+        hostName: hostName,
+        companyName: 'AmpliFury',
+    })
+})

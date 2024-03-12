c908804a5e2c1a3c7e2b7e98916837769fd9aeb6
François-Xavier Savary <fx.savary@orange.fr>
2024-03-12 18:20:09 +0100

    cartIcon badge

--- src/sub-components/CartIcon.vue
+++ src/sub-components/CartIcon.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 
 import { Icon } from '@iconify/vue';
+import { useCatalogueStore } from '@/stores/CatalogueStore';
+import { computed } from 'vue';
 
 const toggleShoppingCart = (): void => {
     // crée un évènement personnalisé
@@ -11,8 +13,59 @@ const toggleShoppingCart = (): void => {
     window.dispatchEvent(showModalOverlay);
 };
 
+// calcule et retourne le nombre d'articles présent dans shoppingCart
+const cartItemsCount = computed<number>(() => {
+    return useCatalogueStore().cartItems.length;
+})
+
 </script>
 
 <template>
-    <Icon icon="bi:cart" @click="toggleShoppingCart"/>
+    <div class="iconAndBadge-container">
+        <Icon icon="bi:cart" @click="toggleShoppingCart" class="icon"/>
+        <div class="badge" v-if="cartItemsCount > 0">
+            <p>{{ cartItemsCount }}</p>
+        </div>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+
+@import '@/assets/colors.scss';
+
+.iconAndBadge-container {
+    position: relative;
+    height: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    &:hover > .icon {
+        color: $accent1;
+    }
+
+    .icon {
+        font-size: 1.4rem;
+    }
+
+    .badge {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 1rem;
+        height: 1rem;
+        background:  $accent1;
+        border-radius: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        p {
+            color: $whiteColor;
+            font-size: .8rem;
+            font-weight: 600;
+        }
+    }
+}
+</style>

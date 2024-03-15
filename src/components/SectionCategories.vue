e4892259e3374b37c1aca3708b877a1d8bea0ed9
François-Xavier Savary <fx.savary@orange.fr>
2024-03-15 17:40:01 +0100

    HomePage 1

--- src/components/SectionCategories.vue
+++ src/components/SectionCategories.vue
@@ -21,21 +21,26 @@ const categories: Category[] = catalogueStore.categories;
 </template>
 
 <style lang="scss" scoped>
-
 @import '@/assets/cardImageContainer.scss';
 
 .categoryCards-container {
     width: 100%;
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
     gap: 1rem;
 
     .category-card {
         width: 100%;
+        height: 25rem;
         display: inline-block;
 
         .image-container {
+            height: 100%;
             display: block;
+
+            img {
+                height: 100%;
+            }
         }
     }
 }
